13ece485ca6e739a4028de691458c9325bdadba5
Artur Kordowski <artur.kordowski@gmail.com>
2016-08-17 19:14:32 +0200

    Add new Item Templates and Snippets.

---
+++ ItemTemplates/1031/Code/AbstractClass/AbstractClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace $rootnamespace$
+{
+	/// <summary>
+	/// Stellt eine abstrakte <see cref="$safeitemname$"/>-Klasse bereit.
+	/// </summary>
+	public abstract class $safeitemrootname$
+	{
+		#region Constructor
+		/// <summary>
+		/// Initialisiert eine neue Instanz der <see cref="$safeitemname$"/>-Klasse.
+		/// </summary>
+		protected $safeitemname$()
+		{
+		}
+		#endregion
+	}
+}
---
+++ ItemTemplates/1033/Code/AbstractClass/AbstractClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace $rootnamespace$
+{
+	/// <summary>
+	/// Provides a abstract <see cref="$safeitemname$"/> class.
+	/// </summary>
+	public abstract class $safeitemrootname$
+	{
+		#region Constructor
+		/// <summary>
+		/// Initializes a new instance of the <see cref="$safeitemname$"/> class.
+		/// </summary>
+		protected $safeitemname$()
+		{
+		}
+		#endregion
+	}
+}
