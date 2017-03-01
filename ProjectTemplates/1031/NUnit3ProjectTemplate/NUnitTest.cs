528efcf8c668bd5d4b737c544c7536c383b48997
Artur Kordowski <artur.kordowski@gmail.com>
2017-03-01 19:37:20 +0100

    Add Snippets and NUnit 3 Project Template.

---
+++ ProjectTemplates/1031/NUnit3ProjectTemplate/NUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace $safeprojectname$
+{
+    [TestFixture]
+    public class NUnitTest
+    {
+        #region Tests
+        [Test]
+        public void TestMethod()
+        {
+        }
+        #endregion
+    }
+}
